8d66e035ad0ee8ae0e358891728ef4f8ab41d751
Zhivche Gligorov <zhivche.gligorov@valtech.com>
2025-10-30 13:41:33 +0100

    feat: integrate middlewares to app

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,37 @@
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express, { type Express } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import routes from '@/features/v1/index';
+
+import { RouteNotFoundError } from './errors/route-not-found.error';
+import { errorHandler } from './middleware/error-handler.middleware';
+import { configureSession } from './middleware/session.middleware';
 
 const app: Express = express();
 
-app.get('/', (req, res) => {
-  res.send('Initial route...');
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(configureSession());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/v1', routes);
+
+app.get(['/*x', '/'], () => {
+  throw new RouteNotFoundError(
+    'The route does not exist or has been deleted!',
+    404,
+  );
 });
+
+app.use(errorHandler);
 
 export default app;
