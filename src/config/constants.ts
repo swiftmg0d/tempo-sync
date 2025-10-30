8b14d17ef5f028b24a150579d8da428e82d67bf7
Zhivche Gligorov <zhivche.gligorov@valtech.com>
2025-10-30 13:45:06 +0100

    chore: add constants for uris

---
+++ src/config/constants.ts
@@ -0,0 +1,4 @@
+export const SPOTIFY_AUTH_URI = 'https://accounts.spotify.com/authorize?';
+export const SPOTFIY_TOKEN_URI = 'https://accounts.spotify.com/api/token';
+export const STRAVA_AUTH_URI = 'http://www.strava.com/oauth/authorize?';
+export const STRAVA_TOKEN_URI = 'https://www.strava.com/oauth/token';
