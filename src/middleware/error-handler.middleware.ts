f358d93f69d520ffb7de17e442e091d588393947
Zhivche Gligorov <zhivche.gligorov@valtech.com>
2025-10-30 13:43:46 +0100

    feat : add error handler middleware

---
+++ src/middleware/error-handler.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+
+interface CustomError extends Error {
+  statusCode?: number;
+}
+
+export const errorHandler = (
+  err: CustomError,
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.error('Error:', err.message);
+  const status = err?.statusCode || 500;
+  res.status(status).json({
+    message: err.message,
+    status: status,
+    success: false,
+  });
+  next();
+};
