56e64a8ebc605c12dbe0d312b971831db77ab27e
Zhivche Gligorov <zhivche.gligorov@valtech.com>
2025-11-09 17:29:26 +0100

    chore(api): add paths for Spotify and Strava APIs

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config({});
+
+const { CLIENT_ID, CLIENT_SECRET } = process.env;
+
 export const SPOTIFY_AUTH_URI = 'https://accounts.spotify.com/authorize?';
 export const SPOTFIY_TOKEN_URI = 'https://accounts.spotify.com/api/token';
 export const STRAVA_AUTH_URI = 'http://www.strava.com/oauth/authorize?';
 export const STRAVA_TOKEN_URI = 'https://www.strava.com/oauth/token';
+export const STRAVA_API_URI = 'https://www.strava.com/api/v3';
+export const SPOTIFY_API_URI = 'https://api.spotify.com/v1';
+export const SPOTIFY_AUTH_HEADER = `Basic ${Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')}`;
