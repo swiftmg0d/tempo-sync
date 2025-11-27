c11320a2a30fa200d82f30896200495cb1cd62a7
swiftmg0d <zhivche.gligorov@gmail.com>
2025-11-28 00:41:07 +0100

    feat: update wrangler configuration and add local example for hyperdrive

--- api/src/middleware/api-key.middleware.ts
+++ api/src/middleware/api-key.middleware.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 
 import { X_API_KEY } from '@/config/env';
+import { success } from 'zod';
 
 export const apiKeyMiddleware = (req: Request, res: Response, next: NextFunction) => {
 	const apiKey = req.headers['x-api-key'];
 
 	if (apiKey && apiKey === X_API_KEY) next();
 
-	return res.status(403).json({ message: 'Forbidden: Invalid API Key' });
+	return res.status(403).json({ message: 'Forbidden: Invalid API Key', success: false, code: 403 });
 };
